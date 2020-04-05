90e61952f3a21c7d149ea7d86dfca93d135e1b49
chris <chris.dz@gmail.com>
2020-04-04 22:41:12 -0400

    ABOUT

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Modal} from 'react-bootstrap'
+import { Modal, Row } from "react-bootstrap";
 class About extends React.Component {
   render() {
     return (
@@ -19,7 +19,27 @@ class About extends React.Component {
             How to use Pigeon?
           </Modal.Title>
         </Modal.Header>
-        <Modal.Body></Modal.Body>
+        <Modal.Body>
+        <Row>
+          Pigeon Support is a free to use ad platform that aims to help those at
+          risk during COVID19 and those who are looking to make an extra income
+          by delivering groceries.
+          </Row>
+          <Row>
+          If you are an individual at risk, please register on our website, and
+          click on Create Order button to submit a list of shopping items you'd
+          like to perform. A delivery person will then call you and pick up
+          their groceries for you. Make sure you have cash and tips ready when
+          the delivery man arrives. We recommend $10 gas money + %10 of total as
+          tips, but you can discuss this with your delivery person.
+          </Row>
+          <Row>
+          If you are an indivdual looking to make some extra income during the
+          bad economic times, simply sign up and our map will reveal contact
+          information of anyone that needs shopping done. It is up to you to
+          negotiate how much you are charging for the delivery.
+          </Row>
+        </Modal.Body>
       </Modal>
     );
   }
